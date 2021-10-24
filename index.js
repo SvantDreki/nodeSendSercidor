52159c68aa43ac110e125a9a5ff793c71956002c
SvantDreki <diazfigueroa91@gmail.com>
2021-10-24 15:02:27 -0400

    cambio cor6

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const opciones = {
 }
 //Habilitar cors
 app.use( cors({ 
-    origin: 'https://blissful-hopper-3134e9.netlify.app', 
+    origin: {source: 'https://blissful-hopper-3134e9.netlify.app'}, 
     optionsSuccessStatus: 200,
     allowedHeaders: 'https://blissful-hopper-3134e9.netlify.app',
     credentials: true
